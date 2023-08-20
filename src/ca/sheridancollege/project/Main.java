62e684c00aae1f68090c9484c34e947731e09f7e
pakv <pakv@Viktoriya.cogeco.local>
2023-08-19 23:11:44 -0400

    final product

---
+++ src/ca/sheridancollege/project/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author pakv
+ */
+public class Main {
+    public static void main(String[] args){
+        War war = new War("Name");
+        war.play();
+    }
+}
